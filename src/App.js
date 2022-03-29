c7bb20fed9b3262d1429de0df6b1b24a1ae33467
w4xccino <dtoledoa@ulasalle.edu.pe>
2022-03-29 16:14:29 -0500

    cambiado el App.js test

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Ok, a la mierda, empezemos con esta porqueria!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Rolandouuu!, para editar este codigo, entrar a <code> App.js </code></p>
       </header>
     </div>
   );
