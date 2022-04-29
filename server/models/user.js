6b4adb21c70b8f69fd3e97f795bf74645e2782a9
w4xccino <dbtoledoa@gmail.com>
2022-04-29 10:42:14 -0500

    Revert "using gitignore"
    This reverts commit 34e4089401c7ac0e2192cf192a660ce35d37d47b.

--- server/models/user.js
+++ server/models/user.js
@@ -1,41 +1,44 @@
 // Inclims el modulo sequelize
-const Sequelize = require("sequelize");
-
+const Sequelize = require('sequelize')
+  
 // Requerimos de la conexion de la BD
-const sequelize = require("../utils/database");
+const sequelize = require('../utils/database')
+  
 
-const Usuario = sequelize.define("usuario", {
-  id: {
-    // Indicamos el tipo de dato INTEGER
-    type: Sequelize.INTEGER,
-
-    // Indicamos que sera autoincrementable
-    autoIncrement: true,
-
-    // El campo id no estara vacio
-    allowNull: false,
-
-    // Indicamos que sera de llave primaria
-    primaryKey: true,
-  },
-
-  // Columna name
-  name: { type: Sequelize.STRING, allowNull: false },
-
-  // Columna, email
-  email: { type: Sequelize.STRING, allowNull: false },
-
-  // columna, password
-  password: { type: Sequelize.STRING, allowNull: false },
-
-  // columna, direccion
-  direccion: { type: Sequelize.STRING, allowNull: false },
-
-  //columna, telefono
-  telefono: Sequelize.INTEGER,
-});
-
+const Usuario = sequelize.define('usuario', {
+  
+    id:{
+  
+        // Indicamos el tipo de dato INTEGER
+        type:Sequelize.INTEGER,
+  
+        // Indicamos que sera autoincrementable
+        autoIncrement:true,
+  
+        // El campo id no estara vacio
+        allowNull:false,
+  
+        // Indicamos que sera de llave primaria
+        primaryKey:true
+    },
+  
+    // Columna name
+    name: { type: Sequelize.STRING, allowNull:false },
+  
+    // Columna, email
+    email: { type: Sequelize.STRING, allowNull:false },
+  
+    // columna, password
+    password: { type: Sequelize.STRING, allowNull:false },
+  
+     // columna, direccion
+    direccion: { type: Sequelize.STRING, allowNull:false },
+    
+     //columna, telefono
+    telefono: Sequelize.INTEGER,
+})
+  
 // Exporting User, using this constant
 // we can perform CRUD operations on
 // 'user' table.
-module.exports = Usuario;
+module.exports = Usuario
