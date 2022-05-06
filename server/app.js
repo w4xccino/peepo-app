24f03c791fb28b2fcc6f78f1bb65df44cacbba35
w4xccino <dbtoledoa@gmail.com>
2022-05-05 21:41:14 -0500

    code cleaned

--- server/app.js
+++ server/app.js
@@ -1,33 +1,39 @@
 const sequelize = require("./utils/database");
-const User = require("./models/user");
+const user = require("./models/user");
 const express = require("express");
 const cors = require("cors");
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); //middleware
 app.use(cors()); //important
 
+//testing db connection
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("Connection to Database Successful!");
+  })
+  .catch((err) => {
+    console.log("Error connecting to database");
+  });
+
+//login api
 app.post("/api/login", (req, res) => {
-  //theres a bug here
   let { email, password } = req.body;
   let values = [email, password];
-  // const values = { nombre: "Diego", email: "jasdjkla@gmail.com" };
-  // res.send(200, { ...values });
   console.log(values);
   res.send("pa eso?");
 });
 
+//register api
 app.post("/api/register", (req, res) => {
-  //theres a bug here
   let { email, password } = req.body;
   let values = [email, password];
-  // const values = { nombre: "Diego", email: "jasdjkla@gmail.com" };
-  // res.send(200, { ...values });
   console.log(values);
 });
 
-sequelize.sync();
 app.listen(4000, () =>
   console.log("Servidor iniciado en http://localhost:4000")
 );
@@ -35,6 +41,5 @@ app.listen(4000, () =>
 /*Para crear el modelo, podemos utilizar una de las siguientes formas:
 Método sync(): Este método creará un modelo si el modelo no existe, sin embargo, si ya existe, no lo sobrescribirá.
 sync ({force: true}) Método: Este método creará un modelo si el modelo no existe, sin embargo, si ya existe, lo sobrescribirá.*/
-sequelize.sync();
 
 //sequelize.sync({force:true})
--- server/models/user.js
+++ server/models/user.js
@@ -1,43 +1,33 @@
-// Inclims el modulo sequelize
+// Inclimos el modulo sequelize
 const Sequelize = require("sequelize");
 
 // Requerimos de la conexion de la BD
 const sequelize = require("../utils/database");
 
-const Usuario = sequelize.define("usuario", {
-  id: {
-    // Indicamos el tipo de dato INTEGER
-    type: Sequelize.INTEGER,
-
-    // Indicamos que sera autoincrementable
-    autoIncrement: true,
-
-    // El campo id no estara vacio
-    allowNull: false,
-
-    // Indicamos que sera de llave primaria
-    primaryKey: true,
-  },
-
+const User = sequelize.define("user", {
   // Columna name
   name: { type: Sequelize.STRING, allowNull: false },
-
   last_name: { type: Sequelize.STRING, allowNull: false },
-
   // Columna, email
   email: { type: Sequelize.STRING, allowNull: false },
-
   // columna, password
-  password: { type: Sequelize.STRING, allowNull: true },
-
+  password: { type: Sequelize.STRING, allowNull: false },
   // columna, direccion
-  address: { type: Sequelize.STRING, allowNull: false },
-
+  address: { type: Sequelize.STRING, allowNull: true },
   //columna, telefono
-  telefono: Sequelize.INTEGER,
+  telefono: { type: Sequelize.STRING, allowNull: true },
 });
+
+User.sync()
+  .then(() => {
+    console.log("table and model synced successful ");
+  })
+  .catch((err) => {
+    console.log("Error syncing the table and model", err);
+  });
 
 // Exporting User, using this constant
 // we can perform CRUD operations on
 // 'user' table.
-module.exports = Usuario;
+
+module.exports = User;
