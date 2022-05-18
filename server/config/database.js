6189c59949b42a81811d21a0a4739a2435dd814d
ronyWil <caceresrony714@gmail.com>
2022-05-18 10:20:32 -0500

    acrhivos ignorados

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-  username: process.env.DB_USERNAME,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  host: process.env.DB_HOST,
+  username: process.env.DB_USERNAME || 'root',
+  password: process.env.DB_PASSWORD || '12345',
+  database: process.env.DB_DATABASE || 'prac_model_data_dev',
+  host: process.env.DB_HOST || 'localhost',
   dialect: process.env.DB_DIALECT,
   define: {
     timestamps: false, //para evitar agregar el registo de modificacion
