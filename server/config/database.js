9d6620b02e6e7d6abdf0bdf17d7724cb06974077
ronyWil <caceresrony714@gmail.com>
2022-05-22 23:20:38 -0500

    config conex

--- server/config/database.js
+++ server/config/database.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 
 module.exports = {
-  username: process.env.DB_USERNAME || 'root',
-  password: process.env.DB_PASSWORD || '12345',
-  database: process.env.DB_DATABASE || 'tes_senati_proyecto',
-  host: process.env.DB_HOST || 'localhost',
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT || 'mysql',
   define: {
     timestamps: false, //para evitar agregar el registo de modificacion
