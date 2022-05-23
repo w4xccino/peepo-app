6c35367e34e2134ebc868f89c2cd0402bb8478ba
ronyWil <caceresrony714@gmail.com>
2022-05-22 23:04:38 -0500

    modelo detalle factura

---
+++ server/App/models/DetalleCompra.js
@@ -0,0 +1,18 @@
+'use strict';
+const {Model} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class DetalleCompra extends Model {}
+  DetalleCompra.init({
+    id_compra:{
+        type:DataTypes.INTEGER,                
+        allowNull:false
+    },
+    cantidad: DataTypes.FLOAT,
+    precio:DataTypes.DECIMAL(10,4)
+  }, {
+    sequelize,
+    modelName: 'detalle_compra',
+    tableName:'detalle_compra'           
+  });
+  return DetalleCompra;
+};
