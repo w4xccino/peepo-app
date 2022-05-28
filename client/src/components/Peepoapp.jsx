a17a8ab175c993bf00e591a647ba4f2069693962
Diego Toledo <dbtoledoa@gmail.com>
2022-05-28 00:07:22 -0500

    I got index value when i select a category

--- client/src/components/Peepoapp.jsx
+++ client/src/components/Peepoapp.jsx
@@ -1,9 +1,8 @@
-import axios from "axios";
 import React, { useEffect, useState } from "react";
 import CardProduct from "./templates/card-product";
 import Footer from "./templates/footer";
 import Navbar from "./templates/Navbar.jsx";
-import Filtros from "./templates/filtros.jsx"
+import Filtros from "./templates/filtros.jsx";
 
 function Peepoapp() {
   const [producto, setProduct] = useState([]);
@@ -14,12 +13,16 @@ function Peepoapp() {
   }, []);
 
   //Consumiendo la api de categoria
-  const [categoria, setCategoria] = useState([]);
+  const [categoriaa, setCategoria] = useState([]);
   useEffect(() => {
     fetch("http://localhost:4000/api/categorias")
       .then((response) => response.json())
       .then((json) => setCategoria(json));
   }, []);
+  let list = [];
+  categoriaa.map((item, idx) => {
+    return list.push(item.nombre_categoria);
+  });
 
   return (
     <div>
@@ -27,24 +30,10 @@ function Peepoapp() {
       <Navbar />
       {/* Contenedor principal de la pagina */}
       <div className="w-full min-h-screen p-6">
-            <div className="flex flex-wrap justify-center">
-            {/*Retornamos los select de categorias, Estado y precio pi*/}
-            {categoria.map((item, icx) => {
-            return (
-              <div key={icx}>
-                <Filtros 
-                primero={item.nombre_categoria}
-                segundo={item.nombre_categoria}
-              
-                />
-              </div>
-              
-              );
-            })}
-                <Filtros list={["Estado", "Nuevo", "Usado", "Reparado"]} />
-                <Filtros list={["Precio", "Menor", "descuentos", "Mayor"]} />
-            </div>
-
+        <div className="flex flex-wrap justify-center">
+          {/*Retornamos los select de categorias, Estado y precio pi*/}
+          <Filtros list={list} />
+        </div>
 
         <div className="flex flex-wrap justify-center ">
           {" "}
@@ -56,21 +45,12 @@ function Peepoapp() {
                 <CardProduct
                   precio={item.precio}
                   producto={item.descripcion}
-                  /* imagen="polo.jpg" */
                   imagen={item.imagen}
                   link={`/productos/${item.id}`}
                 />
               </div>
             );
           })}
-          {/* <CardProduct precio="900" producto="RTX 3090" imagen="rtx" /> */}
-          {/* <CardProduct precio="23440" producto="Huawei P30" imagen="p30" /> */}
-          {/* <CardProduct precio="680" producto="wofer" imagen="wofer" /> */}
-          {/* <CardProduct precio="200" producto="Zapatillas" imagen="jordan" /> */}
-          {/* <CardProduct precio="40" producto="DVD" imagen="dvd" /> */}
-          {/* <CardProduct precio="60000" producto="NFT Romario" imagen="avatar" /> */}
-          {/* <CardProduct precio="120" producto="Huawei p40" imagen="p302" /> */}
-          {/* <CardProduct precio="8000" producto="Github" imagen="Git" /> */}
         </div>
       </div>
 
--- client/src/components/templates/filtros.jsx
+++ client/src/components/templates/filtros.jsx
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
-
-function Filtros(props){
-    return(
-
-<div className="pa-eso">
-  <select className="select select-bordered select-sm w-28 max-w-xs m-1">
-  <option>{props.primero}</option>
-  <option>{props.segundo}</option>
-  <option>{props.tercero}</option>
-  <option>{props.cuarto}</option>
-  </select>
-</div>
-    );
+function Filtros({ list }) {
+  const [selected, setSelected] = useState(0);
+  const ordenar = () => {
+    let selectBox = document.getElementById("form-select");
+    let selectedValue = selectBox.options[selectBox.selectedIndex].index;
+    setSelected(selectedValue + 1);
+  };
+  console.log(selected);
+  return (
+    <div className="pa-eso">
+      <select
+        id="form-select"
+        onChange={ordenar}
+        className="select select-bordered select-sm w-32 max-w-xs m-2"
+      >
+        {list.map((element, idx) => {
+          return <option key={idx}>{element}</option>;
+        })}
+      </select>
+    </div>
+  );
 }
 
 export default Filtros;
