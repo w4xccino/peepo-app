236db537faa9ea0554e03f4d209a40fb556d0b53
romariou76 <illeniumr@gmail.com>
2022-05-28 00:30:28 -0500

    Agregue los archivos workers para el PWA asi como su registro en index.js, y cree la interfaz de la pagina de pagos por el momento

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Producto from "./components/Producto";
 import { ProtectedRoutes } from "./ProtectedRoutes";
 import User from "./components/User";
 import Mainpage from "./components/Mainpage";
+import Payments from "./components/Payments";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route exact path="/productos/:product_id" element={<Producto />} />
           <Route exact path="/user" element={<User />} />
           <Route exact path="/" element={<Mainpage />} />
+          <Route exact path="/payments" element={<Payments />} />
         </Route>
       </Routes>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -4,9 +4,16 @@ import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Router>
     <App />
   </Router>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
