111627172faaf8111dee61e4427c4e3ea642e4e6
romariou76 <illeniumr@gmail.com>
2022-05-29 14:35:55 -0500

    Agregue el enlace de ModeloAR ademas de 1 producto en la base de datos manualmente xd

--- client/src/components/Producto.jsx
+++ client/src/components/Producto.jsx
@@ -24,6 +24,7 @@ function Producto(props) {
             key={inx}
             className=" w-full min-h-screen flex flex-wrap justify-center p-6 bg-base-100"
           >
+            {console.log(item.modeloRA)}
             <div className="card w-96 h-full bg-white shadow-xl py-12">
               <figure className="px-3 pt-3">
                 <img
@@ -34,7 +35,7 @@ function Producto(props) {
               </figure>
               <div className="card-body">
                 <h2 className="card-title text-3xl text-neutral">
-                  {item.descripcion}
+                  {item.modelo}
                 </h2>
                 <p className="text-3xl text-purple-600">S/ {item.precio}0</p>
                 <h2 className="card-title text-neutral">Descripcion</h2>
@@ -46,9 +47,9 @@ function Producto(props) {
                 <br />
                 <br />
                 <div className="card-actions justify-center">
-                  <button className="btn btn-secondary">
-                    Visualizar en AR
-                  </button>
+                <a href={item.modeloRA}>
+                  <input className="text-gray-900 bg-gradient-to-r from-red-200 via-red-300 to-yellow-200 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-red-100 dark:focus:ring-red-400 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" type="button" value="Visualizar en AR"/>
+                </a>
                     <Link to={`/payments/${product_id}`} className="btn btn-primary">
                       Comprar
                     </Link>
