6505e079bba60c522448d26a6b02fcbe8356f077
romariou76 <illeniumr@gmail.com>
2022-06-05 16:20:13 -0500

    He mejorado la interfaz de los card-products, agregue el boton de signout, y mejore la interfaz de la mainpage, por ultimo agregue y actualize algunas imagenes.

--- client/src/components/templates/Navbar.jsx
+++ client/src/components/templates/Navbar.jsx
@@ -205,7 +205,24 @@ function Home() {
                Categorias
              </a>
            </li>
-
+<li>
+            <a onClick={signOut}>
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+                />
+              </svg>
+              Cerrar Sesion
+            </a>
+          </li>
           
       </ul>
     </div>
